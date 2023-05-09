c085e3abd462d8737cb7bbc31dc28b757591c49c
Stefan <shodgeskluck@gmail.com>
2023-05-08 21:01:59 -0400

    more W that is equally IP

--- src/extension.ts
+++ src/extension.ts
@@ -3,8 +3,10 @@ import { commands, ExtensionContext, } from 'vscode';
 import { updateAllInstancesOfClassName } from './commands/update-all-instances-of-class-name';
 
 export function activate(context: ExtensionContext) {
+  // Set context as a global as some tests depend on it
+  (global as any).testExtensionContext = context;
   const updateAllInstancesOfClassNameCommand = commands.registerCommand('dart-class-name-updater.updateAllInstancesOfClass', (uri: vscode.Uri) => {
-      updateAllInstancesOfClassName(uri);    
+    updateAllInstancesOfClassName(uri);
   });
   context.subscriptions.push(updateAllInstancesOfClassNameCommand);
 }
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -6,8 +6,9 @@ suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
 	test('activating adds command to subscriptions', async () => {
-		const extension = vscode.extensions.getExtension("dart-class-name-updater");
-		const context = await extension!.activate() as vscode.ExtensionContext;
+		let context: vscode.ExtensionContext;
+		context = (global as any).testExtensionContext;
+		extension.activate(context);
 
 		assert.equal(context.subscriptions.length, 1);
 	});
