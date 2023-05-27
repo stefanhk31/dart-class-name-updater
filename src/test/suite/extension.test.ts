fb890dd2f8c9a7f29ea2848d476dad80d09b217f
Stefan <shodgeskluck@gmail.com>
2023-05-27 13:02:25 -0400

    stubbing out failing asserts in unimplemented tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -21,48 +21,47 @@ suite('Extension Test Suite', () => {
 
 	group('commands', () => {
 		test('updating calls rename file', async () => {
-
+			assert.equal(true, false);
 		});
 
 		test('updating calls update instances on the updated file', async () => {
-
+			assert.equal(true, false);
 		});
 
 		test('updating calls update instances on all files in project', async () => {
-
+			assert.equal(true, false);
 		});
 	});
 
 	group('name updater', () => {
 		test('can be instantiated', async () => {
-
+			assert.equal(true, false);
 		});
 
 		test('creates snake case name on instantiation', async () => {
-
+			assert.equal(true, false);
 		});
 
 		test('can rename file', async () => {
-
+			assert.equal(true, false);
 		});
 
 		test('can update instances of classname in file', async () => {
-
+			assert.equal(true, false);
 		});
 	});
 
 	group('utils', () => {
 		test('can implement case object interface', async () => {
-
+			assert.equal(true, false);
 		});
 
 		test('can get excluded folders', async () => {
-
+			assert.equal(true, false);
 		});
 
 		test('can convert input to pascal case', async () => {
-
+			assert.equal(true, false);
 		});
 	});
-
 });
