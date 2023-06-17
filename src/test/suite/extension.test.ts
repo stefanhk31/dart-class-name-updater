ea1f853f771f65050a28a72025b64c6fccaa3f61
Stefan <shodgeskluck@gmail.com>
2023-06-17 13:58:58 -0400

    more test progress

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,12 +1,15 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 import * as extension from '../../extension';
-import { anyOfClass, anyString, anything, instance, mock, when } from 'ts-mockito';
+import { anyOfClass, anyString, anything, instance, mock, notNull, when } from 'ts-mockito';
 import { CommandManager } from '../../commands/command-manager';
 import { IVsCodeClient, VsCodeClient } from '../../services/vscode-client';
 import { MockTextDocument } from '../fixtures/mock-text-document';
 import { filePath, newFilePath, newUri, uri } from '../fixtures/constants';
 import { mockVsCodeClient } from '../fixtures/mock-vs-code-client';
+import { CaseObject } from '../../utils/case-object';
+import { inputToPascalCase } from '../../utils/input-to-pascal-case';
+import { existsSync } from 'fs';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
@@ -52,30 +55,27 @@ suite('Extension Test Suite', () => {
 		});
 	});
 
-	suite('name updater', () => {
-		test('can be instantiated', async () => {
-			assert.equal(true, false);
-		});
-
-		test('creates snake case name on instantiation', async () => {
-			assert.equal(true, false);
-		});
-
-		test('can rename file', async () => {
-			assert.equal(true, false);
-		});
-
-		test('can update instances of classname in file', async () => {
-			assert.equal(true, false);
-		});
-	});
-
 	suite('utils', () => {
 		suite('case object', () => {
 			test('can implement case object interface', async () => {
-				assert.equal(true, false);
+				const impl: CaseObject = {
+					camel: (_) => '',
+					capital:  (_) => '',
+					constant: (_) => '',
+					header: (_) => '',
+					kebab: (_) => '',
+					lower: (_) => '',
+					pascal: (_) => '',
+					sentence: (_) => '',
+					snake: (_) => '',
+					title: (_) => '',
+					upper: (_) => '',
+					of: (_) => ''
+				};
+				assert.equal(!impl, false);
 			});
 		});
+
 
 		suite('get excluded folders', () => {
 			test('can get excluded folders', async () => {
@@ -92,9 +92,32 @@ suite('Extension Test Suite', () => {
 		});
 
 		suite('input to pascal case', () => {
-			test('can convert input to pascal case', async () => {
-				assert.equal(true, false);
+			test('can convert input with spaces to pascal case', async () => {
+				assert.equal(inputToPascalCase('my test input'), 'MyTestInput');
+			});
+			
+			test('can convert input camel case to pascal case', async () => {
+				assert.equal(inputToPascalCase('myTestInput'), 'MyTestInput');
+			});
+
+			// TODO (#32): this is failing because it's currently unsupported.
+			// Let's get it passing.
+			test('can convert input snake case to pascal case', async () => {
+				assert.equal(inputToPascalCase('my_test_input'), 'MyTestInput');
+			});
+
+			test('preserves pascal case when input is pascal', async () => {
+				assert.equal(inputToPascalCase('MyTestInput'), 'MyTestInput');
 			});
 		});
+
+		suite('rename file', () => {
+
+		});
+
+		suite('update all instances', () => {
+
+		});
+
 	});
 });
